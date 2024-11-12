a307e441b107824593b97606cd0825962c8deaf7
Ronja <ronja.syrjala@outlook.com>
2024-11-12 10:51:05 +0200

    create a test api router

---
+++ api/controller/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (red, res) => {
+  res.json({ message: "Api is working!" });
+};
--- api/models/user.moodel.js
+++ api/models/user.moodel.js
@@ -1,18 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mangoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        require: true,
-    }
-})
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
